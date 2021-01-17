ad1cc3c85a732f61cf1bc197e86b9e7f0f92859a
Artem <64410376+Artiss90@users.noreply.github.com>
2021-01-17 13:09:07 +0200

    start npm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import React, { Component } from 'react';
+import style from './App.module.css';
 
-function App() {
-  return <div className="App">Всё работает!!!</div>;
+class App extends Component() {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return <div className={style.head}></div>;
+  }
 }
 
 export default App;
