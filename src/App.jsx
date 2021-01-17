d911b6ebb68775ff6a9bfef596cb8171449b9223
Artem <64410376+Artiss90@users.noreply.github.com>
2021-01-17 21:25:59 +0200

    setions complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-/* eslint react/prop-types: 1 */
-
 import { Component } from 'react';
+import Sections from './Components/Sections';
 import Feedback from './Components/Feedback';
-import PropTypes from 'prop-types';
-import style from './App.module.css';
 
 class App extends Component {
-  static defaultProps = { title: 'Please leave feedback' };
-  static propTypes = { title: PropTypes.string };
-
-  state = {
-    title: this.props.title,
-  };
-
   render() {
-    const { title } = this.state;
     return (
-      <div className={style.head}>
-        {title && <h1>{title}</h1>}
+      <Sections title="Please leave feedback">
         <Feedback />
-      </div>
+      </Sections>
     );
   }
 }
--- src/Components/Sections/Sections.jsx
+++ src/Components/Sections/Sections.jsx
@@ -1,3 +1,21 @@
-{
-  /* <Section title=""></Section> */
+/* eslint react/prop-types: 1 */
+
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './Sections.module.css';
+
+export default class Sections extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node,
+  };
+  render() {
+    const { title, children } = this.props;
+    return (
+      <section className={style.head}>
+        {title && <h2>{title}</h2>}
+        {children}
+      </section>
+    );
+  }
 }
